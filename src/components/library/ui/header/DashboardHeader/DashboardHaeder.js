0b381653eee0e02d0fa40e0ab2a0d1ca09fd5615
Gyubin <gbcompany27@gmail.com>
2022-06-22 17:34:02 +0900

    add logo to mobile header

--- src/components/library/ui/header/DashboardHeader/DashboardHaeder.js
+++ src/components/library/ui/header/DashboardHeader/DashboardHaeder.js
@@ -1,11 +1,17 @@
 import './DashboardHeader.css';
 import StockSearchInput from '../../stock/StockSearchInput/StockSearchInput';
+import ImageContainer from '../../unit/ImageContainer/ImageContainer';
 
 export default function DashboardHeader(props) {
     return (
         <div className='header-container'>
             <div className='header-search-input'>
                 <StockSearchInput></StockSearchInput>
+            </div>
+            <div className='header-logo-conatiner'>
+                <ImageContainer
+                    imageHeight='30px'
+                    image={require('../../images/OruumLogo/logo_oruum.png')}></ImageContainer>
             </div>
         </div>
     )

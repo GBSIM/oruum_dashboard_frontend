10912094e941ff13782e1ac3f92f788926cb18e4
Gyubin <gbcompany27@gmail.com>
2022-06-22 19:04:35 +0900

    fix the mistyping

--- src/components/library/ui/stock/StockPriceBlock/StockPriceBlock.js
+++ src/components/library/ui/stock/StockPriceBlock/StockPriceBlock.js
@@ -9,7 +9,7 @@ export default function StockPriceBlock() {
             <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
                 <h2>{currency}{currentPrice}</h2>
                 <div style={{minWidth:'8px'}}></div>
-                <h2 style={{color:'#34B199'}}>+{lastDailyChange}({absoluteLastChangeRate})%</h2>
+                <h2 style={{color:'#34B199'}}>+{lastDailyChange}({absoluteLastChangeRate}%)</h2>
             </div>
         )
     } else {
@@ -17,7 +17,7 @@ export default function StockPriceBlock() {
             <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
                 <h2>{currency}{currentPrice}</h2>
                 <div style={{minWidth:'8px'}}></div>
-                <h2 style={{color:'#F15E5E'}}>{lastDailyChange} ({absoluteLastChangeRate})%</h2>
+                <h2 style={{color:'#F15E5E'}}>{lastDailyChange} ({absoluteLastChangeRate}%)</h2>
             </div>
         )
     }
